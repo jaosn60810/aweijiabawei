5c7c4ed9fe6490faa16f982502a9360bc44b0ef5
jason60810 <jason60810@gmail.com>
2021-12-29 23:14:44 +0800

    上架到一半會員頁壞掉

--- src/firebase.js
+++ src/firebase.js
@@ -13,6 +13,15 @@ var firebaseConfig = {
   appId: '1:234432297202:web:07f72bdf86d04f8ddb786b',
   measurementId: 'G-43S50WF5KX',
 };
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDTFFg4Mf_ozu0FGIqy8Y6js7N1C9G2Yhg',
+//   authDomain: 'aweijiabawei.firebaseapp.com',
+//   projectId: 'aweijiabawei',
+//   storageBucket: 'aweijiabawei.appspot.com',
+//   messagingSenderId: '104225565119',
+//   appId: '1:104225565119:web:8db5886201fc84249c990a',
+// };
 // Initialize Firebase
 
 const fb = firebase.initializeApp(firebaseConfig);

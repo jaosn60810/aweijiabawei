28cba7015aae2f594d75e29a1038edd83716f1fd
jason60810 <jason60810@gmail.com>
2021-12-30 21:39:58 +0800

    將地圖接上後端和修改樣式

--- src/components/About.vue
+++ src/components/About.vue
@@ -31,89 +31,63 @@
             <select id="areaName" class="form-control" v-model="select.store">
               <option value="all">請選擇動物收容所</option>
               <option
-                :value="a.ShelterName"
-                v-for="a in shelterData.filter((city) => {
-                  if (select.city === 'all') {
-                    return city.CountyName;
-                  } else {
-                    return city.CountyName === select.city;
-                  }
-                })"
-                :key="a.ShelterName"
+                :value="a.shelterName"
+                v-for="a in shelter"
+                :key="a.shelterName"
               >
-                {{ a.ShelterName }}
+                {{ a.shelterName }}
               </option>
             </select>
           </div>
         </div>
         <!-- 各收容所資料 -->
-        <div class="overflow-auto bd-links">
+        <div class="overflow-auto bd-links ">
           <div
-            class="card"
-            v-for="a in shelterData.filter((city) => {
-              if (select.city === 'all' && select.store === 'all') {
-                return city.CountyName;
-              } else if (select.store === 'all') {
-                return city.CountyName === select.city;
-              } else {
-                return city.ShelterName === select.store;
-              }
-            })"
-            :key="a.ShelterName"
+            class="card "
+            :class="{
+              highlight: a.realNumber / a.containMaxNumber < 0.5,
+            }"
+            v-for="a in shelter"
+            :key="a.shelterName"
           >
-            <div class="card-body" @click="showPopup(a.ShelterOrder)">
-              <h6 class="card-title">
-                <strong>{{ a.ShelterName }}</strong>
-              </h6>
-              <p class="card-text">
-                開放認養數量:
-                {{ getAdoptNum(a) }}
+            <div class="card-body" @click="showPopup(a.shelterId)">
+              <p class="card-title h3">
+                <strong>{{ a.shelterName }}</strong>
               </p>
+
               <p class="card-text">
-                狗:
-                {{
-                  animals.filter((data) => {
-                    if (data.shelter_name === a.ShelterName)
-                      return data.animal_kind === '狗';
-                  }).length
-                }}
+                <i class="fas fa-drumstick-bite fa-3x"></i>
               </p>
               <b-progress
-                :max="getAdoptNum(a)"
+                :max="a.containMaxNumber"
                 height="2rem"
                 animated
                 show-value
               >
-                <b-progress-bar :value="getDogNum(a)">
+                <b-progress-bar :value="a.realNumber">
                   <span class="progress-text"
                     >捐款點數進度:
                     <strong
-                      >{{ getDogNum(a) }} / {{ getAdoptNum(a) }}</strong
+                      >{{ a.realNumber }} / {{ a.containMaxNumber }}</strong
                     ></span
                   >
                 </b-progress-bar>
               </b-progress>
 
               <p class="card-text">
-                貓:
-                {{
-                  animals.filter((data) => {
-                    if (data.shelter_name === a.ShelterName)
-                      return data.animal_kind === '貓';
-                  }).length
-                }}
+                <i class="fas fa-briefcase-medical fa-3x"></i>
               </p>
               <b-progress
-                :max="getAdoptNum(a)"
+                :max="a.containMaxNumber"
                 height="2rem"
                 animated
                 show-value
               >
-                <b-progress-bar :value="getCatNum(a)">
+                <b-progress-bar :value="a.realNumber">
                   <span class="progress-text"
                     >捐款點數進度:
                     <strong
-                      >{{ getCatNum(a) }} / {{ getAdoptNum(a) }}</strong
+                      >{{ a.realNumber }} / {{ a.containMaxNumber }}</strong
                     ></span
                   >
                 </b-progress-bar>
@@ -135,7 +109,7 @@
 <script>
 import Cart from '../components/Cart';
 import L from 'leaflet';
-import shelterData from '../assets/data/shelterCity.json';
+// import shelterData from '../assets/data/shelterCity.json';
 import asData from '../assets/data/animal-shelter.json';
 
 let openStreetMap = {};
@@ -145,7 +119,7 @@ export default {
   data: () => ({
     asAPI: 'Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL',
     asData,
-    shelterData,
+    shelterData: [],
     animals: [],
     dataReady: false,
     select: {
@@ -162,46 +136,44 @@ export default {
   },
   computed: {
     cities() {
-      return [...new Set(this.shelterData.map(({ CountyName }) => CountyName))];
-    },
-    area() {
-      const area = this.shelterData.filter(
-        (data) => data.CountyName === this.select.city
-      );
-      return [...new Set(area.map(({ TownName }) => TownName))];
+      return [...new Set(this.shelterData.map(({ city }) => city))];
     },
     shelter() {
       return this.shelterData.filter((shelter) => {
-        if (this.select.store === 'all' && this.select.city === 'all') {
-          return shelter.CountyName;
+        if (this.select.city === 'all' && this.select.store === 'all') {
+          return shelter.shelterName;
+        } else if (this.select.store === 'all') {
+          return shelter.city === this.select.city;
+        } else {
+          return shelter.shelterName === this.select.store;
         }
-        if (this.select.store === 'all') {
-          return shelter.CountyName === this.select.city;
-        }
-        return shelter.ShelterName === this.select.store;
       });
     },
   },
   watch: {
     shelter() {
       this.updateMap();
-      this.updateAnimal();
     },
   },
   methods: {
     cityChange() {
       this.select.store = 'all';
     },
-    ducumentReady() {
+    documentReady() {
       this.shelterData.forEach((data) => {
-        const marker = L.marker([data.LAT, data.LNG], {
-          title: data.ShelterOrder,
+        const marker = L.marker([data.latitude, data.longitude], {
+          title: data.shelterId,
         }).addTo(openStreetMap)
-          .bindPopup(`<p><strong style="font-size: 20px;">${data.ShelterName}</strong></p>
-          地址: ${data.Address}<br>
-          電話: ${data.Tel}<br>
-          <span style="color: #d45345;">${data.Memo}</span><br>
-          認養網頁: <a href="${data.link}" target="_blank">${data.link}</a>`);
+          .bindPopup(`<p><strong style="font-size: 20px;">${
+          data.shelterName
+        }</strong></p>
+          地址: <a :href="https://www.google.com.tw/maps/place/${data.address}"
+                target="_blank" title="Google Map">
+                ${data.address}</a><br>
+          電話: ${data.shelterPhoneNumber}<br>
+          <span style="color: #d45345;">
+          ${new Date(data.updateTime).toLocaleString()}</span><br>
+          `);
         this.markers.push(marker);
       });
     },
@@ -214,26 +186,52 @@ export default {
         }
       });
 
+      const iconsConfig = {
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41],
+      };
+
+      const icons = {
+        green: new L.Icon({
+          iconUrl:
+            'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+          ...iconsConfig,
+        }),
+        red: new L.Icon({
+          iconUrl:
+            'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+          ...iconsConfig,
+        }),
+      };
+
       // add markers
       this.shelter.forEach((data) => {
+        const icon =
+          data.realNumber / data.containMaxNumber < 0.5
+            ? icons.red
+            : icons.green;
         // 透過藥局經緯度疊加標記
-        const marker = L.marker([data.LAT, data.LNG], {
-          title: data.ShelterOrder,
+
+        console.log(data.updateTime.toLocaleString());
+        const marker = L.marker([data.latitude, data.longitude], {
+          title: data.shelterId,
+          icon,
         }).addTo(openStreetMap)
-          .bindPopup(`<p><strong style="font-size: 20px;">${data.ShelterName}</strong></p>
-          地址: ${data.Address}<br>
-          電話: ${data.Tel}<br>
-          <span style="color: #d45345;">${data.Memo}</span><br>
-          認養網頁: <a href="${data.link}" target="_blank">${data.link}</a>`);
+          .bindPopup(`<p><strong style="font-size: 20px;">${
+          data.shelterName
+        }</strong></p>
+          地址: <a href="https://www.google.com.tw/maps/place/${data.address}"
+                target="_blank" title="Google Map">
+                ${data.address}</a><br>
+          電話: ${data.shelterPhoneNumber}<br>
+          <span style="color: #d45345;">${new Date(
+            data.updateTime
+          ).toLocaleString()}</span><br>
+          `);
         this.markers.push(marker);
       });
-    },
-    updateAnimal() {
-      // console.log(this.shelter);
-      const result = this.asData.filter((f) =>
-        this.shelter.filter((d) => d.ShelterName === f.shelter_name)
-      );
-      this.animals = result;
     },
     showPopup(id) {
       this.markers.forEach((marker) => {
@@ -245,31 +243,17 @@ export default {
         }
       });
     },
-    getAdoptNum(a) {
-      return this.animals.filter((data) => data.shelter_name === a.ShelterName)
-        .length;
-    },
-    getDogNum(a) {
-      return this.animals.filter((data) => {
-        if (data.shelter_name === a.ShelterName)
-          return data.animal_kind === '狗';
-      }).length;
-    },
-    getCatNum(a) {
-      return this.animals.filter((data) => {
-        if (data.shelter_name === a.ShelterName)
-          return data.animal_kind === '貓';
-      }).length;
-    },
   },
   mounted() {
-    /**
-    const api = `${this.asAPI}&animal_status=OPEN`;
-    this.$http.get(api).then((response) => {
-      this.asData = response.data;
-      this.dataReady = true;
-    });
-     */
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch('https://finalproject-336509.appspot.com/api/shelter', requestOptions)
+      .then((response) => response.json())
+      .then((result) => (this.shelterData = result))
+      .catch((error) => console.log('error', error));
 
     openStreetMap = L.map('map', {
       center: [24, 120.8],
@@ -284,8 +268,7 @@ export default {
       maxZoom: 20,
     }).addTo(openStreetMap);
 
-    this.ducumentReady();
-    this.updateAnimal();
+    // this.documentReady();
   },
 };
 </script>
@@ -306,10 +289,11 @@ export default {
 
 .card {
   cursor: pointer;
+  background: #e9ffe3;
 }
 
 .card:hover {
-  background-color: #f1f1f1;
+  opacity: 0.7;
 }
 
 .progress-text {
@@ -317,5 +301,9 @@ export default {
   position: absolute;
   z-index: 10;
   transform: translateX(1rem);
+}
+
+.highlight {
+  background: #ff4c4c;
 }
 </style>

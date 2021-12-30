6e54d851fbcafb3146b24f584929f9e8e9cc1193
jason60810 <jason60810@gmail.com>
2021-12-30 09:05:32 +0800

    用自己的firebase.config上架

--- src/firebase.js
+++ src/firebase.js
@@ -3,25 +3,25 @@ import 'firebase/storage';
 require('firebase/firestore');
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: 'AIzaSyDcNL_NKslNrxggDFeS98cEngNz4BKuOsI',
-  authDomain: 'vue-shop-19fa3.firebaseapp.com',
-  databaseURL: 'https://vue-shop-19fa3.firebaseio.com',
-  projectId: 'vue-shop-19fa3',
-  storageBucket: 'vue-shop-19fa3.appspot.com',
-  messagingSenderId: '234432297202',
-  appId: '1:234432297202:web:07f72bdf86d04f8ddb786b',
-  measurementId: 'G-43S50WF5KX',
-};
-
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyDTFFg4Mf_ozu0FGIqy8Y6js7N1C9G2Yhg',
-//   authDomain: 'aweijiabawei.firebaseapp.com',
-//   projectId: 'aweijiabawei',
-//   storageBucket: 'aweijiabawei.appspot.com',
-//   messagingSenderId: '104225565119',
-//   appId: '1:104225565119:web:8db5886201fc84249c990a',
+// var firebaseConfig = {
+//   apiKey: 'AIzaSyDcNL_NKslNrxggDFeS98cEngNz4BKuOsI',
+//   authDomain: 'vue-shop-19fa3.firebaseapp.com',
+//   databaseURL: 'https://vue-shop-19fa3.firebaseio.com',
+//   projectId: 'vue-shop-19fa3',
+//   storageBucket: 'vue-shop-19fa3.appspot.com',
+//   messagingSenderId: '234432297202',
+//   appId: '1:234432297202:web:07f72bdf86d04f8ddb786b',
+//   measurementId: 'G-43S50WF5KX',
 // };
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDTFFg4Mf_ozu0FGIqy8Y6js7N1C9G2Yhg',
+  authDomain: 'aweijiabawei.firebaseapp.com',
+  projectId: 'aweijiabawei',
+  storageBucket: 'aweijiabawei.appspot.com',
+  messagingSenderId: '104225565119',
+  appId: '1:104225565119:web:8db5886201fc84249c990a',
+};
 // Initialize Firebase
 
 const fb = firebase.initializeApp(firebaseConfig);
